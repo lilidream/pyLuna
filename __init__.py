99a8ce0425c9957c1576c8ed6a6f28e13fd880fa
Lilidream <37211837+lilidream@users.noreply.github.com>
2020-07-08 22:16:17 +0800

    Update math functions

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# pyLuna, a module about science.
+# 一个存放气象或数学统计相关的计算方法的模组
+# Build by Lilidream. 2020-03-11
+# Email: ubuntullmx@hotmail.com
--- math.py
+++ math.py
@@ -1,20 +1,3 @@
-def standardized(array, r=None):
-    '''
-    标准化变量函数
-    :param array:(list)一组需要标准化操作的数组
-    :param r: (int)四舍五入保留r位小数，不填则完全返回
-    :return: (list)标准化操作后的数据
-    '''
-    import math
-    avg = sum(array) / len(array)
-    ano = [i - avg for i in array]
-    std = math.sqrt(sum([i ** 2 for i in ano]) / (len(array) - 1))
-    if r:
-        return [round(i / std, r) for i in ano]
-    else:
-        return [i / std for i in ano]
-
-
 def mapper(x, x1, x2, X1, X2):
     '''
     一维线性映射函数，返回在x1-x2中的x，映射到X1-X2后的值。
@@ -24,9 +7,12 @@ def mapper(x, x1, x2, X1, X2):
 
 
 def mapper_array(array, x1, x2, X1, X2):
+    '''
+    数组映射，将数组数值从[x1,x2]映射至[X1,X2]
+    :return: npArray
+    '''
     import numpy as np
     array = np.array(array)
-
     def m(x, x1, x2, X1, X2): return ((X2 - X1) * (x - x1) / (x2 - x1)) + X1
     vfunc = np.vectorize(m)
     return vfunc(array, x1, x2, X1, X2)
@@ -71,10 +57,10 @@ def spacial_freq(x, y, minX, maxX, minY, maxY, xSpace, ySpace, value=None, grid=
 
 def t_test(a, b):
     '''
-    计算矩阵序列双变量t统计量值
-    :param a: 以时间为第一维度的平面数据矩阵
-    :param b: 同上
-    :return:
+    两个正态总体的t双侧检验，计算t值
+    :param a: list
+    :param b: list
+    :return:t value
     '''
     import numpy as np
     a = np.array(a)
@@ -159,13 +145,19 @@ def Gaussian(array, karray):
 
 
 def information_diffusion(y, u):
+    '''
+    信息扩散法计算
+    :param y: list 观测值
+    :param u: list 论域
+    :return: list 各个论域值的概率
+    '''
     b = max(y)
     a = min(y)
     m = len(y)
 
     # Calculate h
     if m < 5:
-        raise "Too less examples!"
+        raise Exception("Too less examples!")
     elif m == 5:
         h = 0.8146 * (b - a)
     elif m == 6:
@@ -205,3 +197,122 @@ def information_diffusion(y, u):
         q.append(sum(qq))
     p = [i/sum(q) for i in q]
     return p
+
+class AHP:
+    RI = [0,0,0.52,0.89,1.12,1.26,1.36,1.41,1.46,
+    1.49,1.52,1.54,1.56,1.58,1.59]
+
+    def __init__(self,m):
+        '''
+        层次分析法
+        :param m: 判断矩阵
+        '''
+        import numpy as np
+        n = len(m)
+        Mi = []
+        for i in range(n):
+            mm = 1
+            for j in range(n):
+                mm *= m[i][j]
+            Mi.append(mm)
+        wb = [i**(1/n) for i in Mi]
+        wi = sum(wb)
+        w = np.array([i/wi for i in wb])
+        self.weight = w
+        l = np.dot(m,w)
+        l2 = l/w/n
+        lmax = sum(l2)
+        self.lambda_Max = lmax
+        self.CI = (lmax - n)/(n - 1)
+        self.CR = self.CI / self.RI[n-1]
+
+
+class GRA:
+    def std(self,array,t):
+        mi = min(array)
+        ma = max(array)
+        if t == 1:
+            data = [(i-mi)/(ma-mi) for i in array]
+        elif t == 2:
+            data = [(ma - i)/(ma - mi) for i in array]
+        elif t == 3:
+            data = [i/array[0] for i in array]
+        return data
+
+    def __init__(self,m,rho=0.5,t=3,omega=None):
+        '''
+        灰色关联度
+        :param m: 数据矩阵，第一行为目标相关值，其余为相关因子值
+        :param t: 标准化种类
+        :param rho: rho值，常取0.5
+        :param omega: None or list, omega相关权重，None为平均权重
+        '''
+        import numpy as np
+        l = len(m)
+        k = len(m[0])
+        data = np.array([self.std(m[i],t) for i in range(l)])
+        diff = np.array([np.abs(data[0] - data[i+1]) for i in range(l-1)])
+        ma = np.max(diff)
+        mi = np.min(diff)
+        r0 = np.array([(mi + rho * ma)/(diff[i] + rho * ma) for i in range(l-1)])
+        if omega == None:
+            omega = 1 / k
+            r = [omega * sum(r0[i]) for i in range(l-1)]
+        else:
+            r = [omega[i] * sum(r0[i]) for i in range(l - 1)]
+        self.relate = r
+        self.weight = np.array(r) / sum(r)
+
+
+class standardized:
+
+    def std(self,array,r=None):
+        '''
+        标准化数列，距平/方差
+        :param array:输入数列
+        :param r:四舍五入位数(round)
+        :return:标准化序列
+        '''
+        import math
+        avg = sum(array) / len(array)
+        ano = [i - avg for i in array]
+        std = math.sqrt(sum([i ** 2 for i in ano]) / (len(array) - 1))
+        if r:
+            return [round(i / std, r) for i in ano]
+        else:
+            return [i / std for i in ano]
+
+    # 正向指标
+    def p_indicator(self,array):
+        mi = min(array)
+        ma = max(array)
+        return [(i - mi) / (ma - mi)for i in array]
+
+    # 负向指标
+    def n_indicator(self, array):
+        mi = min(array)
+        ma = max(array)
+        return [(ma - i) / (ma - mi) for i in array]
+
+def EWM(array,stdType="p"):
+    '''
+    熵权法计算权重
+    :param array: 数据二维矩阵
+    :param stdType: 标准化算法,"p"为正向指标,"n"为负向指标
+    :return: [w,s] w为权重list，s为评分list
+    '''
+    import numpy as np
+    std = standardized()
+    if stdType == "p":
+        data = [std.p_indicator(i) for i in array]
+    elif stdType == "n":
+        data = [std.n_indicator(i) for i in array]
+
+    data = np.array(data)
+    data[data == 0] = 0.00001
+    p = np.array([i/np.sum(i) for i in data])
+    e = np.array([(-1/np.log(len(data[0]))) * sum(i*np.log(i)) for i in p])
+    d = 1-e
+    w = d/sum(d)
+    s = [sum(data[i]*w[i]) for i in range(len(data))]
+    return [w,s]
